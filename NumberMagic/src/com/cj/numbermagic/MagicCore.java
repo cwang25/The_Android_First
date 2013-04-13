796af5ee9dbc13a3d8c509fb8f9fdff31b2a9e08
Chi-Han Wang <chihan09nc@gmail.com>
2013-04-13 01:14:58 -0400

    Added three layout.
    Three layout the first versions ( not a good graphic ones :P)

--- NumberMagic/src/com/cj/numbermagic/MagicCore.java
+++ NumberMagic/src/com/cj/numbermagic/MagicCore.java
@@ -1,10 +1,20 @@
 package com.cj.numbermagic;
-
+/**
+ * The dummy magic core object.
+ * @author Chi-Han Wang
+ * @author Jonathan Poston
+ *
+ */
 public class MagicCore {
 	private int digitsTotal;
 	private boolean even;
 	private final int NINE = 9;
 	private int answer;
+	/**
+	 * The constructor of the core object. Which will receive the number  then find the answer.
+	 * @param number The number that user put in.
+	 * @param even If the number is even then true otherwise false.
+	 */
 	public MagicCore(int number, boolean even){
 		this.even = even;
 		String tempnum = Integer.toString(number);
@@ -25,7 +35,10 @@ public class MagicCore {
 		}
 		
 	}
-	
+	/**
+	 * Method to get the answer.
+	 * @return anwer.
+	 */
 	public int getAnswer(){
 		return answer;
 	}
