21af57f21784adcf635407b872ee31f962f04f8b
Jonathan Poston <jfposton@gmail.com>
2013-06-01 08:09:21 -0400

    Extracted a few strings to comform to Android coding style.

--- KingsCup/gen/com/cj/kingscup/R.java
+++ KingsCup/gen/com/cj/kingscup/R.java
@@ -82,6 +82,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_next_card=0x7f040006;
+        public static final int button_restart=0x7f040005;
+        public static final int card_description=0x7f040004;
+        public static final int cards_rule=0x7f040003;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
