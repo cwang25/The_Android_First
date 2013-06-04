f2bd068dd186f6c7659a4e294aba74ad0905cb93
Jonathan Poston <jfposton@gmail.com>
2013-06-03 23:57:19 -0400

    Implemented the constructor for KingsCupSystem

--- KingsCup/src/com/cj/kingscup/KingsCupSystem.java
+++ KingsCup/src/com/cj/kingscup/KingsCupSystem.java
@@ -2,12 +2,16 @@ package com.cj.kingscup;
 
 import java.util.Random;
 
-/**	
- * The main system class that will hold mostly the all game's functions here. Then let MainActivity class to call the method that will interact with user.
+/**
+ * The main system class that will hold mostly the all game's functions here.
+ * Then let MainActivity class to call the method that will interact with user.
+ * 
  * @author Chi-Han Wang
  * @author Jonathan Poston
  */
 public class KingsCupSystem {
+	private static final int CARDS_IN_SUITS = 13;
+	private static final int NUM_OF_SUITS = 4;
 	/**
 	 * The array of the cards, The deck that will be used.
 	 */
@@ -21,55 +25,59 @@ public class KingsCupSystem {
 	 */
 	public final int CARDS_IN_DECK = 52;
 	/**
-	 * The index that will act like pointer for showing where the cards that user is picking.
+	 * The index that will act like pointer for showing where the cards that
+	 * user is picking.
 	 */
 	private int index;
-	
+
 	/**
 	 * The object of the system, start on the whole system.
 	 */
-	public KingsCupSystem(){
-		index = 0;
+	public KingsCupSystem() {
 		deck = new Card[CARDS_IN_DECK];
-		for(int i =0; i<CARDS_IN_DECK;i++){
-			//Create new card
-			//put new card in deck
+		char[] suits = { Card.CLUBS, Card.DIAMONDS, Card.HEARTS, Card.SPADES };
+		for (int i = 0; i < NUM_OF_SUITS; i++) {
+			for (int j = 0; j <= CARDS_IN_SUITS; j++) {
+				Card temp = new Card(j+2, suits[i], null);
+				deck[j+(12*(i))] = temp;
+			}
 		}
 		shuffle();
-		
-		
+
 	}
-	
-	
 
 	/**
 	 * shuffle the deck, which also means restart the game.
 	 */
-	public void shuffle(){
-		//Create a new Random number generator
+	public void shuffle() {
+		// Create a new Random number generator
 		Random rand = new Random();
-		//swap cards at random places shuffle_swaps number of times
-		for(int i = 0 ; i < shuffle_swaps ; i ++){
-			//pick two random numbers 
-			swap(rand.nextInt(CARDS_IN_DECK-1),rand.nextInt(CARDS_IN_DECK-1));
+		// swap cards at random places shuffle_swaps number of times
+		for (int i = 0; i < shuffle_swaps; i++) {
+			// pick two random numbers
+			swap(rand.nextInt(CARDS_IN_DECK - 1),
+					rand.nextInt(CARDS_IN_DECK - 1));
 		}
 		index = 0;
 	}
+
 	/**
 	 * Swaps two cards A and B
+	 * 
 	 * @param int one of the cards to swap
 	 * @param int one of the cards to swap
 	 */
-	public void swap(int A, int B){
+	public void swap(int A, int B) {
 		Card temp = deck[A];
 		deck[A] = deck[B];
 		deck[B] = temp;
 	}
+
 	/**
 	 * Gets the next card
 	 */
-	public Card getNextCard(){
+	public Card getNextCard() {
 		return deck[index++];
-		
+
 	}
 }
