91da15ee9c5c938b02236b85e75bce26b7f547b7
Chi-Han Wang <chihan09nc@gmail.com>
2013-06-09 11:46:39 -0400

    added some animator classes into project
    I just added some anumator classes which can be used for our card
    flipping.
    and I am still working on the how to really run those package.
    Feel free to read this website (where I got those information from)
    :http://developer.android.com/training/animation/cardflip.html
    
    and due to some freaking  ..... GITHUB problem that  the SmallMagic
    project was corrupted ... just disregard the changes other than
    KingsCup~~~  since we dont use Small Magic anymore....
    
    Sorry for those confusion.
    :)

--- KingsCup/gen/com/cj/kingscup/R.java
+++ KingsCup/gen/com/cj/kingscup/R.java
@@ -8,6 +8,12 @@
 package com.cj.kingscup;
 
 public final class R {
+    public static final class animator {
+        public static final int card_flip_left_in=0x7f040000;
+        public static final int card_flip_left_out=0x7f040001;
+        public static final int card_flip_right_in=0x7f040002;
+        public static final int card_flip_right_out=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -66,28 +72,32 @@ public final class R {
         public static final int spade9=0x7f020034;
     }
     public static final class id {
-        public static final int cardrule=0x7f070003;
-        public static final int imageView1=0x7f070004;
-        public static final int menu_settings=0x7f070005;
-        public static final int nextcard=0x7f070001;
-        public static final int restart=0x7f070002;
-        public static final int start=0x7f070000;
+        public static final int cardrule=0x7f090003;
+        public static final int imageView1=0x7f090004;
+        public static final int menu_settings=0x7f090005;
+        public static final int nextcard=0x7f090001;
+        public static final int restart=0x7f090002;
+        public static final int start=0x7f090000;
+    }
+    public static final class integer {
+        public static final int card_flip_time_full=0x7f050000;
+        public static final int card_flip_time_half=0x7f050001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int gamescreen=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int button_next_card=0x7f040006;
-        public static final int button_restart=0x7f040005;
-        public static final int card_description=0x7f040004;
-        public static final int cards_rule=0x7f040003;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f060000;
+        public static final int button_next_card=0x7f060006;
+        public static final int button_restart=0x7f060005;
+        public static final int card_description=0x7f060004;
+        public static final int cards_rule=0x7f060003;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
     }
     public static final class style {
         /** 
@@ -111,10 +121,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- NumberMagic/gen/com/cj/numbermagic/R.java
+++ NumberMagic/gen/com/cj/numbermagic/R.java
@@ -46,9 +46,10 @@ public final class R {
         public static final int textView1=0x7f090003;
     }
     public static final class layout {
-        public static final int first_stage=0x7f030000;
-        public static final int second_stage=0x7f030001;
-        public static final int third_stage=0x7f030002;
+        public static final int activity_main=0x7f030000;
+        public static final int first_stage=0x7f030001;
+        public static final int second_stage=0x7f030002;
+        public static final int third_stage=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
