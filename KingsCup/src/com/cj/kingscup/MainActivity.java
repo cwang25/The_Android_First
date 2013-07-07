5d60844d6f84a2fa80a169d86c27cfa2bd2c68e6
Chi-Han Wang <chihan09nc@gmail.com>
2013-07-07 16:23:40 -0400

    is not working
    gonna fixed it later on

--- KingsCup/src/com/cj/kingscup/MainActivity.java
+++ KingsCup/src/com/cj/kingscup/MainActivity.java
@@ -8,14 +8,25 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-
+	private boolean mShowingBack = false;
+	ImageView imgview = (ImageView) findViewById(R.id.imageView1);
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gamescreen);
+		if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.imageView1, new CardFrontFragment())
+                    .commit();
+        }
+//		imgview.setOnClickListener((OnClickListener) this);
+		
+		
 	}
 
 	@Override
@@ -54,5 +65,48 @@ public class MainActivity extends Activity {
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 			return inflater.inflate(R.drawable.club0, container, false);
 		}
+	}
+	
+	private void flipCard() {
+	    if (mShowingBack) {
+	        getFragmentManager().popBackStack();
+	        return;
+	    }
+
+	    // Flip to the back.
+
+	    mShowingBack = true;
+
+	    // Create and commit a new fragment transaction that adds the fragment for the back of
+	    // the card, uses custom animations, and is part of the fragment manager's back stack.
+
+	    getFragmentManager()
+	            .beginTransaction()
+
+	            // Replace the default fragment animations with animator resources representing
+	            // rotations when switching to the back of the card, as well as animator
+	            // resources representing rotations when flipping back to the front (e.g. when
+	            // the system Back button is pressed).
+	            .setCustomAnimations(
+	                    R.animator.card_flip_right_in, R.animator.card_flip_right_out,
+	                    R.animator.card_flip_left_in, R.animator.card_flip_left_out)
+
+	            // Replace any fragments currently in the container view with a fragment
+	            // representing the next page (indicated by the just-incremented currentPage
+	            // variable).
+	            .replace(R.id.imageView1, new CardBackFragment())
+
+	            // Add this transaction to the back stack, allowing users to press Back
+	            // to get to the front of the card.
+	            .addToBackStack(null)
+
+	            // Commit the transaction.
+	            .commit();
+	}
+	
+	public void onClick(View v){
+		if(v == imgview){
+    		flipCard();
+    	}
 	}
 }
